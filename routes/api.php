6f8865b4d475c7cd45587470888ea8b786235acb
ammar <amarya211fgff@gmail.com>
2026-03-03 21:21:10 +0200

    start search

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\SearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::post('/doctor-search' , [SearchController::class , 'search_for_doctor']);
