e27c949f14bfd41ecb62bd5681b682be62b2af7d
Mig0o0 <Mig0o0@protonmail.com>
2026-03-10 15:47:49 +0200

    Feat: Add Missing Routes, Some Alert for Success Operation

--- resources/views/doctor/patients/edit.blade.php
+++ resources/views/doctor/patients/edit.blade.php
@@ -3,6 +3,12 @@
 
 @section('content')
     <div class="container-fluid pt-4 px-4">
+        @if(session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
         <div class="d-flex justify-content-between mb-4">
             <h5 class="mb-0">Edit Patient</h5>
             <a href="{{ route('doctor.patients.show', $patient->id) }}" class="btn btn-sm btn-primary">Back</a>
@@ -16,7 +22,7 @@
                         <div class="col-md-3 text-center"> <img
                                 src="https://static.vecteezy.com/system/resources/thumbnails/020/416/057/small/man-icon-vector.jpg"
                                 class="rounded-circle mb-3 border" style="width:120px;height:120px;object-fit:cover;">
-                            <div> <input type="file" class="form-control form-control-sm"> </div>
+                            <div> <input name="photo_url" type="file" class="form-control form-control-sm"> </div>
                         </div> <!-- Form Fields -->
                         <div class="col-md-9">
                             <div class="row g-3">
