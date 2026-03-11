691e035d70a2b38b43486ebcf770b66fbd6bc4cb
anashamdi10 <anashamdi76@gmail.com>
2026-03-11 22:52:33 +0200

    resolve sidebar

--- resources/views/layout/Sidebar.blade.php
+++ resources/views/layout/Sidebar.blade.php
@@ -30,104 +30,37 @@
           <a href="{{ route('bookingtable') }}" class="nav-item nav-link"><i class="fa-solid fa-table"></i>Booking</a>
          
           
-          <div class="navbar-nav w-100">
-               @if(auth()->check() && auth()->user()->role === 'admin')
-                    <a  href="{{ route('admin.doctors.create') }}" class="nav-item nav-link "><i class="fa fa-tachometer-alt me-2"></i>Add Doctor</a>
-                    <a class="nav-link" href="{{ route('admin.specialties.index') }}">Specialties</a>
-                    <div class="nav-item dropdown">
-                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>FAQS and Policies</a>
-                         <div class="dropdown-menu bg-transparent border-0">
-                              <a href="/admin/Faqs" class="dropdown-item">FAQS</a>
-                              <a href="/admin/Policies" class="dropdown-item">Policies</a>
-                              
-                         </div>
-                    </div>
-                    <div class="nav-item dropdown">
-                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-users"></i></i>Reports</a>
-                         <div class="dropdown-menu bg-transparent border-0">
-                              <a href="/admin/doctor-reviews-report" class="dropdown-item">Doctor Review Report</a>
-                              <a href="/admin/doctor-booking-report" class="dropdown-item">Doctor Booking Report</a>
-                         
-                         </div>
-                    </div>
-              @endif
-          </div>
+         
           @endif
             @if(auth()->check() && auth()->user()->role=='doctor')
           <a href="{{ route('doctorpanal') }}" class="nav-item nav-link"><i class="fa-solid fa-table"></i>mybooking</a>
            @endif
               @if(auth()->check() && auth()->user()->role=='admin')
-          <a href="{{ route('admin.specialties.index') }}" class="nav-item nav-link"><i class="fa-solid fa-bars"></i>Specialties</a>
+               <a href="{{ route('admin.specialties.index') }}" class="nav-item nav-link"><i class="fa-solid fa-bars"></i>Specialties</a>
           @endif
-          <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
-          <div class="nav-item dropdown">
-               <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
-               <div class="dropdown-menu bg-transparent border-0">
-    <nav class="navbar bg-light navbar-light">
-        <a href="index.html" class="navbar-brand mx-4 mb-3">
-            <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
-        </a>
-        <div class="d-flex align-items-center ms-4 mb-4">
-            <div class="position-relative">
-                <img class="rounded-circle" src=" {{ asset('dashboard/img/user.jpg') }}" alt=""
-                    style="width: 40px; height: 40px;">
-                <div
-                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
-                </div>
-            </div>
-            <div class="ms-3">
-                <h6 class="mb-0">Jhon Doe</h6>
-                <span>Admin</span>
-            </div>
-        </div>
-        <div class="navbar-nav w-100">
-        
-            <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-
-            <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-            <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                        class="fa-solid fa-users"></i></i>Users</a>
-                <div class="dropdown-menu bg-transparent border-0">
-                    <a href="{{ route('showpatient') }}" class="dropdown-item">patient</a>
-                    <a href="{{ route('showdoctor') }}" class="dropdown-item">Doctor</a>
-                </div>
-            </div>
-            <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-            <a href="{{ route('chat') }}" class="nav-item nav-link">
-                <i class="fa fa-comments me-2"></i>Chat
-            </a> <a href="{{ route('bookingtable') }}" class="nav-item nav-link"><i
-                    class="fa-solid fa-table"></i>Booking</a>
-            <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-            <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
-            <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
-            <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                        class="far fa-file-alt me-2"></i>Pages</a>
-                <div class="dropdown-menu bg-transparent border-0">
-                    <a href="signin.html" class="dropdown-item">Sign In</a>
-                    <a href="signup.html" class="dropdown-item">Sign Up</a>
-                    <a href="404.html" class="dropdown-item">404 Error</a>
-                    <a href="blank.html" class="dropdown-item">Blank Page</a>
-               </div>
-          </div>
-        
-          </div>
-     </nav>
-                </div>
-            </div>
+         
+          
             <div class="navbar-nav w-100">
                 @if (auth()->check() && auth()->user()->role === 'admin')
                   
                  
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                                class="fa fa-laptop me-2"></i>FAQS and Policies</a>
+                                class="fa fa-laptop me-2"></i>FAQS & Policies</a>
                         <div class="dropdown-menu bg-transparent border-0">
                             <a href="/admin/Faqs" class="dropdown-item">FAQS</a>
                             <a href="/admin/Policies" class="dropdown-item">Policies</a>
 
                         </div>
+
+                         <div class="nav-item dropdown">
+                              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-chart-bar me-2"></i></i>Reports</a>
+                              <div class="dropdown-menu bg-transparent border-0">
+                                   <a href="/admin/doctor-reviews-report" class="dropdown-item">Doctor Review Report</a>
+                                   <a href="/admin/doctor-booking-report" class="dropdown-item">Doctor Booking Report</a>
+                              
+                              </div>
+                         </div>
                     </div>
                 @endif
             </div>
